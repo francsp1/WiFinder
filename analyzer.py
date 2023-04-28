bd8d4abcd35181518d70df866ceb21f7dbf99394
francsp1 <francsp1@hotmail.com>
2023-04-28 21:45:53 +0100

    fix wifite

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import signal
 import os
 import sys
 import subprocess
@@ -8,60 +9,39 @@ import shutil
 from pynput.keyboard import Controller, Key
 import pandas as pd
 import importlib
-#from git import Repo
+
+def signal_handler(sig, frame):
+    time.sleep(2)
+    
 
 def main():
-    """
-    dependencies = ['iwconfig', 'iw', 'wash', 'hcxtools', 'hcxdumptool', 'macchanger']
 
-    #iremos verificar se tem as dependências para executar o wifite, se não tiver, ele instála-as
-    for dependency in dependencies:
-        try:
-            subprocess.check_output(['which', dependency])
-        except subprocess.CalledProcessError:
-            print(f'{dependency} não está instalado. Instalando...')
-                
-            subprocess.check_call(['sudo', 'apt-get', 'install', '-y', dependency])
-
-
-    #aqui temos de instalar o libtool e fazer o pull do código do francisco
-    subprocess.call(["sudo", "apt-get", "install", "-y", "libtool"])
-
-        #subprocess.call(["sudo", "git", "clone", "https://github.com/francsp1/aircrack-ng.git"])
-    Repo.clone_from("https://github.com/francsp1/aircrack-ng.git", "/home/pi")
-
-    subprocess.call(["cd", "/home/pi/aircrack-ng"])
-    subprocess.call(["autoreconf", "-i"])
-    subprocess.call(["./configure"])
-    subprocess.call(["make")
-    subprocess.call(["make", "install")
-    subprocess.call(["ldconfig"])
-    """
+    signal.signal(signal.SIGINT, signal_handler)    
     print("Hello world")
-    #clear stdout 
+    # Clear stdout 
     sys.stdout.flush()
  
-    command = ["/home/pi/projeto/wifite2/wifite.py", "--all", "--kill", "--skip-crack", "--no-wps", "--no-pmkid", "--clients-only"]
-    wifite_proces = subprocess.Popen(command)
-
-
-    # wait for 3 minutes
-    time.sleep(16)  
-    
-    
+    command = ["python3", "/home/pi/projeto/wifite2/wifite.py", "--all", "--kill", "--skip-crack", "--no-wps", "--no-pmkid", "--clients-only"]
+    wifite_process = subprocess.Popen(command)
+        
+    # Wait for 3 minutes
+    time.sleep(15)
+ 
+    # Copy 
     shutil.copy2(glob.glob('/tmp/wifite*/airodump-01.csv')[0], './')
+    time.sleep(2)  
 
-    # send the CTRL + C keystroke to stop Wifite
+    # Send the CTRL + C keystroke to stop scanning and "all" to attack all networks
     keyboard = Controller()
     keyboard.press(Key.ctrl)
     keyboard.press('c')
     keyboard.release('c')
     keyboard.release(Key.ctrl)
-    
-    time.sleep(4)  
-
+    time.sleep(3)
     keyboard.type('all\n')
+
+    wifite_process.wait()
+ 
 
 if __name__ == "__main__":
     main()
-
