a52cc70df65fccd0a53a31309f4877c7dd0d76cd
francsp1 <francsp1@hotmail.com>
2023-05-04 11:30:50 +0100

    Install our Aircrack-ng version

--- analyzer.py
+++ analyzer.py
@@ -9,23 +9,70 @@ import shutil
 from pynput.keyboard import Controller, Key
 import pandas as pd
 import importlib
+from git import Repo
+
+
+SCAN_TIME = 60
+AIRCRACK_REPO_PATH = "/home/pi/projeto/aircrack-ng"
+WIFI_ANALYZER_PATH = "/home/pi/projeto/wifi_analyzer"
 
 def sigint_handler(sig, frame):
-    time.sleep(2)
+    time.sleep(3)
     
 
 def main():
 
     signal.signal(signal.SIGINT, sigint_handler)    
     print("Hello world")
+
+    
+    if not os.path.isfile(os.path.join(WIFI_ANALYZER_PATH, 'installed.txt')):
+        # Se o arquivo não existir, exclui todo o conteúdo do diretório
+        shutil.rmtree(AIRCRACK_REPO_PATH)
+
+        # Install the libtool package
+        os.system("sudo apt-get install -y libtool")
+
+        # Clone the repository using python-git library
+        repo_url = "https://github.com/francsp1/aircrack-ng.git"
+        local_path = "/home/pi/projeto/aircrack-ng/"
+        Repo.clone_from(repo_url, local_path)
+
+        # Change the working directory to "/home/pi/projeto/aircrack-ng"
+        os.chdir("/home/pi/projeto/aircrack-ng")
+
+        # Run the autoreconf command
+        os.system("autoreconf -i")
+
+        # Run the configure command
+        os.system("./configure")
+
+        # Run the make command
+        os.system("make")
+
+        # Run the make install command
+        os.system("sudo make install")
+
+        file_path = os.path.join(WIFI_ANALYZER_PATH, "installed.txt")
+        with open(file_path, "w") as f:
+            f.write("Aircrack-ng for wifi analyzer is installed!")
+
+        # Update the linker cache
+        os.system("sudo ldconfig")
+    else:
+        print("Aircrack-ng for wifi analyzer is installed!")
+        time.sleep(2)
+
     # Clear stdout 
     sys.stdout.flush()
  
-    command = ["python3", "/home/pi/projeto/wifite2/wifite.py", "--all", "--kill", "--skip-crack", "--no-wps", "--no-pmkid", "--clients-only"]
+    #command = ["python3", "/home/pi/projeto/wifite2/wifite.py", "--all", "--kill", "--skip-crack", "--no-wps", "--no-pmkid", "--clients-only"]
+    command = ["python3", "/home/pi/projeto/wifite2/wifite.py", "--all", "--kill", "--skip-crack", "--no-wps", "--no-pmkid"]
+
     wifite_process = subprocess.Popen(command)
         
     # Wait for 3 minutes
-    time.sleep(15)
+    time.sleep(SCAN_TIME)
  
     # Copy the airodump-ng file created via wifite from tmp directory
     shutil.copy2(glob.glob('/tmp/wifite*/airodump-01.csv')[0], './')
