daed16f0c059870bebd8140cb6161fc10d4baac9
Francisco Pedrosa <53799075+francsp1@users.noreply.github.com>
2023-07-03 16:48:42 +0100

    das

--- start.py
+++ start.py
@@ -26,7 +26,7 @@ GPIO.setup(BLUE_PIN, GPIO.OUT)
 #used to exit program
 process = None
 
-API_IP = '192.168.1.159.231' 
+API_IP = '192.168.159.231' 
 API_PORT = 3000  
 API_URL_CHECK = f'http://{API_IP}:{API_PORT}/check'
 API_URL_UPLOAD = f'http://{API_IP}:{API_PORT}/upload'
